9c04b269cc54af851d3793f454f6e3a333ef5adc
NavarSecurity <cayden@navarsecurity.com>
2026-06-19 11:04:58 -0500

    Removed Exports

--- 02-tenant-backup-deploy-cli/exports/tenant-backup-2026-06-18-1810/actions/Custom Phone Provider/code.js
+++
@@ -1,9 +0,0 @@
-/**
-* Handler to be executed while sending a phone notification
-* @param {Event} event - Details about the user and the context in which they are logging in.
-* @param {CustomPhoneProviderAPI} api - Methods and utilities to help change the behavior of sending a phone notification.
-*/
-exports.onExecuteCustomPhoneProvider = async (event, api) => {
-  // Code goes here
-  return;
-};
